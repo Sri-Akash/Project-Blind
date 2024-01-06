222d5ca9ec7f1b8c45ed107f03b5b9dcdb8eb3ea
unknown <sriakash152@gmail.com>
2024-01-06 02:44:55 -0800

    Detecting object via Webcam

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import cv2
 
-dataset = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# img = cv2.imread('Images/all.jpg')
 
-# For Image
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
-'''image = cv2.imread('Images/2.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+classNames = []
+classFile = 'labels.txt'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
 
-faces = dataset.detectMultiScale(gray)
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
 
-for x, y, w, h in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (235, 81, 23), 2)
+net = cv2.dnn.DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
 
-cv2.imshow('Single', image)'''
-
-# For WebCam
-
-video = cv2.VideoCapture(0)
 while True:
-    success, frame = video.read()
-    if success:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = dataset.detectMultiScale(gray)
-        for x, y, w, h in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (235, 81, 23), 2)
-        cv2.imshow('Video', frame)
-        key = cv2.waitKey(1)
-        if key == 81 or key == 113:
-            break
-    else:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+    print(classIds, bbox)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            if 0 <= classId - 1 < len(classNames):
+                cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30), cv2.FONT_HERSHEY_COMPLEX,
+                            2,
+                            (0, 255, 0), 2, cv2.LINE_AA)
+            else:
+                print("Invalid classId or classNames list is not long enough.")
+
+    cv2.imshow('Output', img)
+    key = cv2.waitKey(1)
+    if key == 27:
         break
+cv2.destroyAllWindows()
